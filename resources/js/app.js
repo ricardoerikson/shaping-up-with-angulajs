51b575098ca682d0feb8d69d2f967aa1c7fab6b6
Ricardo Erikson <ricardoerikson@gmail.com>
2015-03-09 23:14:05 -0400

    Working with angular directives

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,17 +7,33 @@
 	var gems = [
 	{
 		name: 'Dodecahedron',
-		price: 2.95,
+		price: 2,
 		description: 'A description',
 		canPurchase: true,
-		soldOut: false
+		soldOut: false,
+		images: [
+		{
+			full: 'resources/images/01-full.png',
+			thumb: 'resources/images/01-thumb.png'
+		},
+		{
+			full: 'resources/images/02-full.png',
+			thumb: 'resources/images/02-thumb.png'
+		}
+		]
 	},
 	{
 		name: 'Pentagonal Gem',
 		price: 5.95,
 		description: 'Another description',
 		canPurchase: false,
-		soldOut: false
+		soldOut: false,
+		images: [
+		{
+			full: 'resources/images/03-full.png',
+			thumb: 'resources/images/03-thumb.png'
+		}
+		]
 	}
 	];
 })();
