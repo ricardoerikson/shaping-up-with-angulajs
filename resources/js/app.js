bbc0e618454ac03274011efa6212283fe2a6e3e8
Ricardo Erikson <ricardoerikson@gmail.com>
2015-03-10 01:20:03 -0400

    Finished level 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,21 @@
 (function () {
 	var app = angular.module('store', [ ]);
+
 	app.controller('StoreController',function(){
 		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
 	});
 
 	var gems = [
